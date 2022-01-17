4cf3900c92ebf3a83ffc431478de0665b32f208b
razcoen <raz@sprkl.dev>
2022-01-17 14:36:24 +0200

    return api to init method

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('sprkl')({ serviceName: 'mern-crud', agent: {host: process.env.OTEL_COLLECTOR}});
+require('sprkl').init({ serviceName: 'mern-crud', agent: {host: process.env.OTEL_COLLECTOR}});
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
